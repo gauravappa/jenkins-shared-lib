46fc27cbb8d9f71d2f122a4fe6a8e524071973d2
Gaurav Appa <42839054+gauravappa@users.noreply.github.com>
2026-09-19 07:10:56 +0530

    Fix syntax for docker tag and push commands

--- vars/publishImage.groovy
+++ vars/publishImage.groovy
@@ -6,8 +6,8 @@ def call(String credentialsId,String imageName,String imageVersion){
           )]) {
                 echo "Starting Image Publishing"    
                 sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
-                sh "docker tag \$imageName:\$imageVersion \$DOCKER_USER/\$imageName:\$imageVersion"
-                sh "docker push  \$DOCKER_USER/\$imageName:\$imageVersion"
+                sh "docker tag \"${imageName}\":\"${imageVersion}\" \$DOCKER_USER/\"${imageName}\":\"${imageVersion}\""
+                sh "docker push  \$DOCKER_USER/\"${imageName}\":\"${imageVersion}\""
                 echo "Completed Image Publishing"  
               }
 }
